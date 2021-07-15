af81991a01d94827cbad073147f4b4915d74efe5
Bill Glick <bglick@illinois.edu>
2021-07-15 09:33:52 -0500

    SECURITY-1298: Remove SUID from fusermount binaries

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,9 @@
 #   - github.com/ncsa/puppet-profile_docker
 #   - puppetlabs/firewall
 #
+# @param files_remove_setuid
+#   Hash of file resource paramters that need setuid removed from them
+#
 # @param firewall_allow_from
 #     Array[ String, 1 ]
 #     Open the firewall for all "sources" in this list
@@ -12,17 +15,25 @@
 # @example
 #   include profile_puppet_master
 class profile_puppet_master (
-    Array[ String, 1 ] $firewall_allow_from,
+  Hash               $files_remove_setuid,
+  Array[ String, 1 ] $firewall_allow_from,
+
 ) {
 
-    # allow incoming on port 8140
-    $firewall_allow_from.each | String $cidr | {
-        firewall { "500 profile::ncsa::puppet_master - puppet access from '${cidr}'" :
-            proto  => 'tcp',
-            dport  => '8140',
-            action => 'accept',
-            source => $cidr,
-        }
+  # Remove setuid/setgid from key files
+  $file_remove_setuid_defaults = {
+    mode    => 'ug-s',
+  }
+  ensure_resources('file', $files_remove_setuid, $file_remove_setuid_defaults )
+
+  # allow incoming on port 8140
+  $firewall_allow_from.each | String $cidr | {
+    firewall { "500 profile::ncsa::puppet_master - puppet access from '${cidr}'" :
+      proto  => 'tcp',
+      dport  => '8140',
+      action => 'accept',
+      source => $cidr,
     }
+  }
 
 }
